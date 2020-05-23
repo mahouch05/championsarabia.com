b38f72b00a5350e7d1fde93a4a1cfff2db3a549d
mahouch05 <51887827+mahouch05@users.noreply.github.com>
2020-05-23 06:53:54 +0100

    Added new files

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+$server = "sql208.epizy.com";
+$username = "epiz_25840217";
+$password = "YUtZqyKrfHHzcHI";
+$dbname = "epiz_25840217_championsenglish";
+
+$conn = mysqli_connect($server, $username, $password, $dbname)
+
+if(!^conn) {
+    die("Connection Failed").mysqli_connect_error());
+}
+
+?>
+
